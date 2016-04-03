f1358f468e615a7729802c10aef255072a891904
sergiopeluzzi <sergiopeluzzi@gmail.com>
2016-04-03 13:13:03 -0400

    Auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,20 +10,20 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-// Album Routes
-Route::get('/', ['as' => 'index','uses' => 'AlbumsController@getList']);
-Route::get('/createalbum', ['as' => 'create_album_form','uses' => 'AlbumsController@getForm']);
-Route::post('/createalbum', ['as' => 'create_album','uses' => 'AlbumsController@postCreate']);
-Route::get('/deletealbum/{id}', ['as' => 'delete_album','uses' => 'AlbumsController@getDelete']);
-Route::get('/album/{id}', ['as' => 'show_album','uses' => 'AlbumsController@getAlbum']);
+Route::group(['middleware' => 'auth'], function() {
+    // Album Routes
+    Route::get('/', ['as' => 'index','uses' => 'AlbumsController@getList']);
+    Route::get('/createalbum', ['as' => 'create_album_form','uses' => 'AlbumsController@getForm']);
+    Route::post('/createalbum', ['as' => 'create_album','uses' => 'AlbumsController@postCreate']);
+    Route::get('/deletealbum/{id}', ['as' => 'delete_album','uses' => 'AlbumsController@getDelete']);
+    Route::get('/album/{id}', ['as' => 'show_album','uses' => 'AlbumsController@getAlbum']);
 
 // Image Routes
-Route::get('/addimage/{id}', ['as' => 'add_image','uses' => 'ImagesController@getForm']);
-Route::post('/addimage', ['as' => 'add_image_to_album','uses' => 'ImagesController@postAdd']);
-Route::get('/deleteimage/{id}', ['as' => 'delete_image','uses' => 'ImagesController@getDelete']);
-Route::post('/moveimage', ['as' => 'move_image', 'uses' => 'ImagesController@postMove']);
-
+    Route::get('/addimage/{id}', ['as' => 'add_image','uses' => 'ImagesController@getForm']);
+    Route::post('/addimage', ['as' => 'add_image_to_album','uses' => 'ImagesController@postAdd']);
+    Route::get('/deleteimage/{id}', ['as' => 'delete_image','uses' => 'ImagesController@getDelete']);
+    Route::post('/moveimage', ['as' => 'move_image', 'uses' => 'ImagesController@postMove']);
+});
 // Galery Routes
 Route::get('/gallery', ['as' => 'gallery','uses' => 'ImagesController@getGallery']);
 Route::get('/gallery/{id}', ['as' => 'photo_gallery','uses' => 'ImagesController@getPhotoGallery']);
